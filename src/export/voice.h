122ef2a6f2ff7e2ce0946fe4260bd46fa880e140
Nikita Ushakov <ushakovn@tcd.ie>
2024-03-09 13:13:01 +0000

    Change voice params

--- src/export/voice.h
+++ src/export/voice.h
@@ -1,76 +1,44 @@
 #pragma once
 
-extern "C" PLUGIN_API void SetClientListening(CBaseEntity* receiver, CBaseEntity* sender, int8_t listen)
+extern "C" PLUGIN_API void SetClientListening(int receiver, int sender, int8_t listen)
 {
-	if (!receiver)
-	{
-		g_Logger.Warning("Receiver is a null pointer\n");
-		return;
-	}
-
-	if (!sender)
-	{
-		g_Logger.Warning("Sender is a null pointer\n");
-		return;
-	}
-
-	auto iSenderSlot = sender->GetEntityIndex().Get() - 1;
-	if (iSenderSlot < 0 || iSenderSlot >= gpGlobals->maxClients)
+	if (sender < 0 || sender >= gpGlobals->maxClients)
 	{
 		g_Logger.Warning("Invalid sender\n");
 		return;
 	}
 
-	auto pPlayer = g_PlayerManager.GetPlayerBySlot(receiver->GetEntityIndex().Get() - 1);
+	auto pPlayer = g_PlayerManager.GetPlayerBySlot(receiver);
 	if (pPlayer == nullptr)
 	{
 		g_Logger.Warning("Invalid receiver\n");
 		return;
 	}
 
-	pPlayer->SetListen(iSenderSlot, static_cast<ListenOverride>(listen));
+	pPlayer->SetListen(sender, static_cast<ListenOverride>(listen));
 }
 
-extern "C" PLUGIN_API int8_t GetClientListening(CBaseEntity* receiver, CBaseEntity* sender)
+extern "C" PLUGIN_API int8_t GetClientListening(int receiver, int sender)
 {
-	if (!receiver)
-	{
-		g_Logger.Warning("Receiver is a null pointer\n");
-		return Listen_Default;
-	}
-
-	if (!sender)
-	{
-		g_Logger.Warning("Sender is a null pointer\n");
-		return Listen_Default;
-	}
-
-	auto iSenderSlot = sender->GetEntityIndex().Get() - 1;
-	if (iSenderSlot < 0 || iSenderSlot >= gpGlobals->maxClients)
+	if (sender < 0 || sender >= gpGlobals->maxClients)
 	{
 		g_Logger.Warning("Invalid sender\n");
 		return Listen_Default;
 	}
 
-	auto pPlayer = g_PlayerManager.GetPlayerBySlot(receiver->GetEntityIndex().Get() - 1);
+	auto pPlayer = g_PlayerManager.GetPlayerBySlot(receiver);
 	if (pPlayer == nullptr)
 	{
 		g_Logger.Warning("Invalid receiver\n");
 		return Listen_Default;
 	}
 
-	return pPlayer->GetListen(iSenderSlot);
+	return pPlayer->GetListen(sender);
 }
 
-extern "C" PLUGIN_API void SetClientVoiceFlags(CBaseEntity* client, uint8_t flags)
+extern "C" PLUGIN_API void SetClientVoiceFlags(int clientIndex, uint8_t flags)
 {
-	if (!client)
-	{
-		g_Logger.Warning("Receiver is a null pointer\n");
-		return;
-	}
-
-	auto pPlayer = g_PlayerManager.GetPlayerBySlot(client->GetEntityIndex().Get() - 1);
+	auto pPlayer = g_PlayerManager.GetPlayerBySlot(clientIndex);
 	if (pPlayer == nullptr)
 	{
 		g_Logger.Warning("Invalid receiver\n");
@@ -80,15 +48,9 @@ extern "C" PLUGIN_API void SetClientVoiceFlags(CBaseEntity* client, uint8_t flag
 	pPlayer->SetVoiceFlags(flags);
 }
 
-extern "C" PLUGIN_API uint8_t GetClientVoiceFlags(CBaseEntity* client)
+extern "C" PLUGIN_API uint8_t GetClientVoiceFlags(int clientIndex)
 {
-	if (!client)
-	{
-		g_Logger.Warning("Receiver is a null pointer\n");
-		return 0;
-	}
-
-	auto pPlayer = g_PlayerManager.GetPlayerBySlot(client->GetEntityIndex().Get() - 1);
+	auto pPlayer = g_PlayerManager.GetPlayerBySlot(clientIndex);
 	if (pPlayer == nullptr)
 	{
 		g_Logger.Warning("Invalid receiver\n");
