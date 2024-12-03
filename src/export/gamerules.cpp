9396f5e564785ba40d34b1dd6b544b25bb06023a
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-12-03 22:13:06 +0000

    Add get game rules

---
+++ src/export/gamerules.cpp
@@ -0,0 +1,14 @@
+#include "core/sdk/entity/cgamerules.h"
+#include <plugin_export.h>
+
+extern CCSGameRules* g_pGameRules;
+
+extern "C" PLUGIN_API void* GetGameRules()
+{
+	return g_pGameRules;
+}
+
+/*extern "C" PLUGIN_API void TerminateRound(float flDelay, CSRoundEndReason reason)
+{
+	g_pGameRules->TerminateRound(flDelay, reason);
+}*/
