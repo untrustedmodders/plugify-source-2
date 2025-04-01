f8716415d852dbc918e0de5530809e33614cf010
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-04-01 02:14:23 +0100

    fix: update configs again

--- cmake/plugify-configs.cmake
+++ cmake/plugify-configs.cmake
@@ -5,7 +5,7 @@ message(STATUS "Pulling and configuring plugify-configs")
 FetchContent_Declare(
         plugify-configs
         GIT_REPOSITORY https://github.com/untrustedmodders/plugify-configs.git
-		GIT_TAG 28218969327160ba85a66164e4295b03e2c497f6
+		GIT_TAG a65ec9fd591610ed85523579b717ecd6d9d81c5a
 )
 
 FetchContent_MakeAvailable(plugify-configs)
